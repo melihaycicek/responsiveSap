5e1e672b9f04372b6fe4c346873e192823669f73
mlh aycck <mlhaycck@gmail.com>
2021-08-12 21:07:42 +0000

    gonderim

---
+++ webapp/Component.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"project/model/models"
+], function(UIComponent, models) {
+	"use strict";
+
+	return UIComponent.extend("project.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+		}
+	});
+});
